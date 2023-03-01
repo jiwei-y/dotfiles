538ebf2a8c4b25dadf0ae79330fac54ef360f7e6
Jiwei Yang <yangjiwei@protonmail.com>
2023-03-01 23:36:23 +1000

    1

--- flatpak.sh
+++ flatpak.sh
@@ -1,6 +1,7 @@
 ## font
-cp -rP ~/.guix-home/profile/share/fonts/opentype/* ~/.local/share/fonts --remove-destination
-cp -rP ~/.guix-home/profile/share/fonts/truetype/* ~/.local/share/fonts --remove-destination
+cp -frP ~/.guix-home/profile/share/fonts/opentype/* ~/.local/share/fonts --remove-destination
+cp -frP ~/.guix-home/profile/share/fonts/truetype/* ~/.local/share/fonts --remove-destination
+cp -fLr ~/.guix-home/profile/share/fonts/truetype/Noto*CJK.ttc ~/.local/share/fonts
 rm -f ~/.local/share/fonts/fonts.dir ~/.local/share/fonts/fonts.scale
 readlink ~/.local/share/fonts/NotoColorEmoji.ttf    # fonts-dir
 readlink -f ~/.local/share/fonts/NotoSans-Black.ttf # font-my-noto-core
@@ -8,10 +9,10 @@ readlink -f ~/.local/share/fonts/NotoSansCJK.ttc # font-my-noto-sans-cjk
 readlink -f ~/.local/share/fonts/NotoSerifCJK.ttc # font-my-noto-serif-cjk
 readlink -f ~/.local/share/fonts/NotoColorEmoji.ttf # font-my-noto-emoji
 
-# Then input these two paths into flatseal
+# Then input these paths into flatseal
 
 ## icons(to solve cursor problem in firefox)
-sudo cp -rP /run/current-system/profile/share/icons/Adwaita ~/.local/share/icons --remove-destination
+sudo cp -frP /run/current-system/profile/share/icons/Adwaita ~/.local/share/icons --remove-destination
 readlink ~/.local/share/icons/Adwaita/index.theme    # gtk-icon-themes
 readlink -f ~/.local/share/icons/Adwaita/index.theme    # adwaita-icon-theme
 # Then input these two paths into flatseal
